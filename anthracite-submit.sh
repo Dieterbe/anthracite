92201b8619f1dcc5c27ef6b3018141c161c53cea
Dieter Plaetinck <dieter@vimeo.com>
2013-02-12 18:03:24 -0500

    add script to easily submit events

---
+++ anthracite-submit.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+prog="$(basename $0)"
+
+type=manual_$USER
+server=$HOSTNAME
+anthracite_host=localhost
+anthracite_port=2005
+msg=
+
+function usage () {
+cat << EOF
+usage: $0 options <msg>
+
+Submit a message to anthracite
+with type=$type
+description="server=$server <msg>"
+
+OPTIONS:
+   -h          Show this message
+   -H          anthracite host (default: $anthracite_host)
+   -p          anthracite port (default: $anthracite_port)
+   -s <msg>    submit message
+   
+EOF
+}
+
+function submit () {
+    msg="$(date +%s) $type server=$server $1"
+    echo "submitting to $anthracite_host:$anthracite_port:"
+    echo "$msg"
+    if netcat -c $anthracite_host $anthracite_port <<< "$msg"; then
+        echo ok
+        exit 0
+    else
+        echo failed >&2
+        exit 1
+    fi
+}
+
+while getopts ":hH:p:s:" opt; do
+    case $opt in
+        h) usage && exit 
+            ;;
+        H) anthracite_host=$OPTARG
+            ;;
+        p) anthracite_port=$OPTARG
+            ;;
+        s) msg="$OPTARG"
+            ;;
+        ?) echo "Invalid option: -$OPTARG" >&2; usage >&2; exit 1;;
+    esac
+done
+[ -n "$msg" ] && submit "$msg"
