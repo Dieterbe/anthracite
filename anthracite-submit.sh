2131ae2ef2cb1f445c74580ea8f9f4dc82b7f5a2
Dieter Plaetinck <dieter@plaetinck.be>
2013-03-14 22:56:22 -0400

    do submission based on editable template file

--- anthracite-submit.sh
+++ anthracite-submit.sh
@@ -1,58 +1,91 @@
 #!/bin/bash
 prog="$(basename $0)"
 
-type=manual_$USER
-server=$HOSTNAME
 anthracite_host=localhost
-anthracite_port=2005
-date="$(date +%s)"
-msg=
+anthracite_port=8081
+tmp_file_tpl=/tmp/anthracite-submit-${USER}.
 
 function die_error () {
     echo "$1" >&2
+    if [ -n "$template_file" -a -f "$template_file" ]; then
+        echo "template file: $template_file" >&2
+    fi
     exit 2
+}
+
+template () {
+cat << EOF
+# date can be any value accepted by 'date -d'
+# no strict format.  anything is allowed (tags, unstructured text, ..)
+# if the event (i.e. this file) is empty, submission will be aborted.
+# everything upto the line below will be ignored
+# please type your event message below. what happened?
+date=$(date)
+user=$USER
+type=manual
+source_host=$HOSTNAME
+source=$prog
+EOF
+}
+
+template_get_content () {
+    sed '1,/# please type your event message below/d'
+}
+
+template_filter_date () {
+    sed -n 's#^date=##p'
 }
 
 function usage () {
 cat << EOF
-usage: $0 options <msg>
+== USAGE ==
+invocation: $0 options
 
-Submit a message to anthracite
-date=$date [$(date -d "@$date")]
-type=$type
-description="server=$server <msg>"
+Gives you an editable message and submits it to an anthracite server, if you want
 
 OPTIONS:
    -h          Show this message
    -H          anthracite host (default: $anthracite_host)
    -p          anthracite port (default: $anthracite_port)
-   -d <datestr> use date described by <datestr> (date -d '<datestr>')
-   -s <msg>    submit message
-   
+   -s          submit message
+   -t <file>   start from template <file>
+
+== MESSAGE TEMPLATE: ==
 EOF
+template
 }
 
 function submit () {
-    local msg=$1
-    [ -n "$msg" ] || die_error "message must be non-zero"
-    msg="$date $type server=$server $1"
-    echo "submitting to $anthracite_host:$anthracite_port:"
-    echo "$msg"
-    # note:
-    # netcat: netcat -c $anthracite_host $anthracite_port <<< "$msg"
-    # on centos, both bash and netcat approach work
-    # debian bash is compiled without support for /dev/tcp
-    # debian netcat doesn't support this syntax; without -c it stays open, waiting
-    if echo "$msg" > /dev/tcp/$anthracite_host/$anthracite_port; then
-        echo ok
-        exit 0
+    [ -n "$EDITOR" ] || die_error "\$EDITOR must be set (protip: export EDITOR=vim in ~/.bashrc)"
+    which $EDITOR > /dev/null || die_error "can't find your editor, $EDITOR"
+    templates=($(shopt -s nullglob; echo $tmp_file_tpl*))
+    if [ ${#templates[@]} -gt 1 ]; then
+        die_error "More than 1 existing template found: ${templates[*]}.  Please clean them up"
+    elif [ ${#templates[@]} -eq 1]; then
+        template_file=${templates[0]}
     else
-        die_error failed
+        template_file=$(mktemp /tmp/anthracite-submit.XXXXX) || die_error "Couldn't make tmpfile"
+        template > $template_file  || die_error "Couldn't write to tmpfile $tmp_file"
+    fi
+    $EDITOR $template_file || die_error "Editor exited $?. aborting..."
+    [ -n "$(cat $template_file | template_get_content)" ] || { rm -f "$template_file" ; die_error "empty file. aborting.." }
+    submission_file=$(mktemp /tmp/anthracite-submit.XXXXX) || die_error "Couldn't make tmpfile"
+    cat $template_file | template_get_content > $submission_file || die_error "Couldn't process template contents"
+    date=$(cat $submission_file | template_filter_date) || die_error "Couldn't find a date in the template. aborting.."
+    timestamp=$(date +%s -d "$date") || die_error "Couldn't parse date from $date. aborting.."
+
+    echo "submitting to $anthracite_host:$anthracite_port:"
+    echo curl -F "event_time=$timestamp;event_type=manual;event_desc=@$submission_file" http://$anthracite_host:$anthracite_port
+    if curl -F 'event_time=$timestamp' - F 'event_type=manual' -F 'event_desc=<$submission_file' http://$anthracite_host:$anthracite_port/add | grep -q 'The new event was added'; then
+        echo "submitted!"
+        exit
+    else
+        die_error "failed"
     fi
 }
 
 action=
-while getopts ":hH:p:s:d:" opt; do
+while getopts ":hH:p:st:" opt; do
     case $opt in
         h) action=usage
             ;;
@@ -61,9 +94,9 @@ while getopts ":hH:p:s:d:" opt; do
         p) anthracite_port=$OPTARG
             ;;
         s) action=submit
-           msg="$OPTARG"
             ;;
-        d) date=$(date -d "$OPTARG" +%s) || die_error "could not run date -d $OPTARG to get a unix timestamp from your date"
+        t) template_file=$OPTARG
+           [ -f "$template_file" ] || die_error "Can't read template file $template_file"
             ;;
         ?) echo "Invalid option: -$OPTARG" >&2; usage >&2; exit 1;;
     esac
@@ -73,5 +106,5 @@ if [ -z "$action" ]; then
 elif [ "$action" == usage ]; then
     usage
 elif [ "$action" == submit ]; then
-    submit "$msg"
+    submit
 fi
