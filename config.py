5efce1e8d4ba954f49d74e8ad0b9b339a5526a46
Dieter Plaetinck <dieter@plaetinck.be>
2013-03-24 13:31:17 -0400

    more simple sample start date

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 listen_host = '0.0.0.0'  # defaults to "all interfaces"
 listen_port = 8081
-opsreport_start = '08/01/2012 12:00:00 AM'
+opsreport_start = '01/01/2013 12:00:00 AM'
 timezone = "America/New_York"
