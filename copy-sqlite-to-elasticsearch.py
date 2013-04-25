c2aade99397c59617ba8e32784ce8c4e7bef5f0b
Dieter Plaetinck <dieter@plaetinck.be>
2013-04-25 11:19:48 -0400

    sleep a bit before counting events in elasticsearch

--- copy-sqlite-to-elasticsearch.py
+++ copy-sqlite-to-elasticsearch.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 from backend import get_backend, Event
+import time
+
 sqlite = get_backend('sqlite')
 es = get_backend('elasticsearch')
 print "sqlite has %i events" % sqlite.get_events_count()
@@ -9,6 +11,8 @@ for (i, event) in enumerate(sqlite.get_events()):
     print "copying event %i: %s" % (i, event)
     es.add_event(event)
 
+print "sleeping to let elasticsearch settle.."
+time.sleep(4)
 print "sqlite has %i events" % sqlite.get_events_count()
 print "elasticsearch has %i events" % es.get_events_count()
 
